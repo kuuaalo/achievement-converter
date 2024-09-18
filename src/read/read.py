055ffc0c7adcfc93134dfeef9f72343b7d2bdf51
vivi <viivi.niinisalo@tuni.fi>
2024-09-18 14:43:38 +0300

    Add more structure to read

--- src/read/read.py
+++ src/read/read.py
@@ -5,3 +5,28 @@
 # achievementien erottelu ja jäsentäminen = separate, parse
 # tietojen muokkaus = edit,
 # datan siirtäminen eteenpäin main -osaan = move
+
+# tähän kerään syntyneitä ajatuksia, ja mahdollisia käytettäviä funktioita ynnä muuta
+
+def identify_file(file_path):
+# 1. identifying the file
+    pass
+
+def read_file(file_path):
+# 2. reading the file
+    pass
+
+def parse_achievements(file_data):
+# 3. structuring the achievements
+    pass
+
+
+def achievements_to_txt(achievements):
+# 4. editing the files
+    pass
+
+# 5. testing the function
+if __name__=="__main__":
+    file_path = "path/to/your/file.xml"
+    result = read_main(file_path)
+    print(result)
