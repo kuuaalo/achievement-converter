0ed28b936e0c1b827f12e5e83ec73f780588af67
vivi <viivi.niinisalo@tuni.fi>
2024-09-18 14:02:46 +0300

    Add structure to read

---
+++ src/read/read.py
@@ -0,0 +1,7 @@
+#readin pitää sisältää osat:
+# tiedoston valinta ja haku käyttäjältä = choose, get, (mainin homma?)
+# tiedoston tunnistaminen = identify
+# tiedoston lukeminen = read,
+# achievementien erottelu ja jäsentäminen = separate, parse
+# tietojen muokkaus = edit,
+# datan siirtäminen eteenpäin main -osaan = move
