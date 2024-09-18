4f2da94fb63267c1274f52af00085f9b71456875
Raulimakinen <rauli.a.makinen@tuni.fi>
2024-09-18 14:45:21 +0300

    User input to main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
+import sys
+platform = None
 def main():
-    print("hello world")
+#komentoriviltä - input(sys.argv)
+
+    order = int(input("Choose 1. To import new project, 2 To open existing project:"))
+    if order == 1:
+        global platform
+        platform = int(input("Choose 1. for Epic, 2. for Steam 3. for Ms Store:"))
+        path = input("Input file path:")
+        print(path)
+        read/read(platform, path)
+    
+    if order == 2:
+        print("Open existing project")
+        path = input("Input file path:")
+        print(path)
+    if order == 3: 
+        exit
+
+
+
 if __name__ == "__main__":
     main()
+print(platform)    
