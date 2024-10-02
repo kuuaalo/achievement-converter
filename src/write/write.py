2e7158b7e605610732084d53a9a4019aed72d9c7
Sonja Tuomi <sonja.tuomi@tuni.fi>
2024-10-02 10:34:34 +0300

    Add init to write

--- src/write/write.py
+++ src/write/write.py
@@ -1,16 +1,26 @@
-import csv
+# aja testi tiedosto, xml python kirjasto? python listan xmlksi
 
-def write_data(array, output_file):
-# Kirjoittaa listan csv-tiedostoon. 
-# Args: array (list of lists): Kirjoitettava taulukko, jossa jokainen alilista edustaa yhtä riviä CSV-tiedostossa.output_file (str): CSV-tiedoston nimi tai polku, johon taulukko kirjoitetaan.
-# Returns: None
+import time
 
-# Yritä kirjoittaa taulukko CSV-tiedostoon
-    try:
-        with open(output_file, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(array)
-        print(f"Tiedot on onnistuneesti kirjoitettu '{output_file}'-tiedostoon.")
-    except IOError:
-        print(f"Virhe: CSV-tiedostoa '{output_file}' ei voitu kirjoittaa.")
+class Write:
+
+    def __init__(self, file_name, format, tero):
+      
+        if file_name: 
+            self.file_name = file_name
+            pass
+        else:
+            print: ("wrong file name")   
+            return False 
+
+        if format:
+            self.format = format
+        else:
+            print: ("wrong format")
+            return False
+
+        
+    def run(self)
+        pass
+
 
