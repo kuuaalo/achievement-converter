01d2535bddda6f641a87918bd3eb20b42f9a0173
Sonja Tuomi <sonja.tuomi@tuni.fi>
2024-10-22 19:46:20 +0300

    Add comments

--- src/write/testi-xml.py
+++ src/write/testi-xml.py
@@ -1,24 +1,29 @@
-from xml.dom.minidom  import Document
+# import the document class for creating XML documents
+from xml.dom.minidom  import Document 
 
+# define a dictionary to store achievement details
 achievement = {
     "achievement1": "true",
     "status": "winner",
     "number": "100"
 }
 
-# luo uuden tiedoston (XML)
+# create a new XML document
 doc = Document ()
 
+# create the root element 'achievement' for the XML document
 root = doc.createElement ('achievement')
+# add the root element to the document
 doc.appendChild(root)
 
+# loop through each key-value pair in the achievement dictionary
 for key, value in achievement.items():
-    item = doc.createElement(key)
+    item = doc.createElement(key) # create an XML element for each key
 
-    item.appendChild(doc.createTextNode(value))
-    root.appendChild(item)
+    item.appendChild(doc.createTextNode(value)) # add the value as a text node to the created element
+    root.appendChild(item) # add the item element to the root element
 
-#tallenna xml-tiedostoon
+# save the XML structure to a file named 'achievements.xml'
 with open("achievements.xml", "w", encoding="utf-8") as f:
     f.write(doc.toprettyxml(indent=" "))
 
