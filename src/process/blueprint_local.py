6f7f5a4cec43c3889c0131c0447f662b26288509
Raulimakinen <rauli.a.makinen@tuni.fi>
2025-01-08 15:13:26 +0200

    Start blueprint for localizations
    still in progress

---
+++ src/process/blueprint_local.py
@@ -0,0 +1,31 @@
+blueprint_local = {
+        "achievement_id": None,
+        "locale": None,
+        "locked_title": None,
+        "locked_description":None,
+        "unlocked_title": None,
+        "unlocked_description":None,
+        "flavor_text": None,
+        "locked_icon": None,
+        "unlocked_icon": None,
+        
+
+        "lang": None,
+        "tokens":None,
+        "NEW_ACHIEVEMENT_1_[i]_NAME": None,
+        "NEW_ACHIEVEMENT_1_[i]_DESC":None,
+
+        "id" :None,
+        "id_locale" : None, #Ms store,text for title, needs some iterating again?
+        "id_locked_desc": None,
+        "locked_translation": None,
+        "id_unlocked_desc": None,
+        "unlocked_translation": None,
+        
+
+
+    }
+
+
+
+#Tarvitaan joku laskuri tai ynnääjä sille, montako riviä steam achievementiin tehdään 1_1, 1_2 
